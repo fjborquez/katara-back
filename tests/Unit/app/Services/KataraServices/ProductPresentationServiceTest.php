705f5a0ef08e7aa308db5518f7bff943a72893fe
Francisco Borquez <fborquez@outlook.com>
2024-12-23 23:50:33 -0300

    unit tests

--- tests/Unit/app/Services/KataraServices/ProductPresentationServiceTest.php
+++ tests/Unit/app/Services/KataraServices/ProductPresentationServiceTest.php
@@ -49,4 +49,26 @@ class ProductPresentationServiceTest extends TestCase
             $this->kataraProductPresentationService->list();
         }, UnexpectedErrorException::class);
     }
+
+    public function test_create_product_presentation_should_create_a_new_product_presentation()
+    {
+        $this->zukoProductPresentationService->shouldReceive('create')->andReturn(new Response(new Psr7Response(HttpFoundationResponse::HTTP_CREATED, ['Location' => url('/api/product-presentation/1')])));
+        $response = $this->kataraProductPresentationService->create(['description' => 'Bottle']);
+        $this->assertEquals(HttpFoundationResponse::HTTP_CREATED, $response['code']);
+    }
+
+    public function test_create_product_presentation_should_return_error_when_unprocessable_entity()
+    {
+        $this->zukoProductPresentationService->shouldReceive('create')->andReturn(new Response(new Psr7Response(HttpFoundationResponse::HTTP_UNPROCESSABLE_ENTITY, [])));
+        $response = $this->kataraProductPresentationService->create(['description' => 'Bottle']);
+        $this->assertEquals(HttpFoundationResponse::HTTP_UNPROCESSABLE_ENTITY, $response['code']);
+    }
+
+    public function test_create_product_presentation_should_throw_an_exception_when_failed()
+    {
+        $this->zukoProductPresentationService->shouldReceive('create')->andReturn(new Response(new Psr7Response(HttpFoundationResponse::HTTP_INTERNAL_SERVER_ERROR, [])));
+        $this->assertThrows(function () {
+            $this->kataraProductPresentationService->create(['description' => 'Bottle']);
+        }, UnexpectedErrorException::class);
+    }
 }
