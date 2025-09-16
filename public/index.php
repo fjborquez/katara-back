7c7e5f6e11543f3e1162fa4b2bbde9c3073a871f
Francisco Bórquez <fborquez@outlook.com>
2025-09-15 21:16:33 -0300

    asdsaa

--- public/index.php
+++ public/index.php
@@ -4,8 +4,6 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-exit(var_dump(date('l jS \of F Y h:i:s A')));
-
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
